8277b72682774144375e173f8c576c9f01dfa6cd
Fran Gette <franngette@gmail.com>
2020-11-30 23:59:57 -0300

    added basic structure

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FlatList, Text } from "react-native";
+import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
+
+const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All Products",
+};
+
+export default ProductsOverviewScreen;
