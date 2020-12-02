48eedbe5186879d8797f1eff522f3b326d992464
Fran Gette <franngette@gmail.com>
2020-12-02 00:32:28 -0300

    added Cart, fonts

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,22 +1,30 @@
-import {createAppContainer } from "react-navigation";
-import {createStackNavigator} from "react-navigation-stack"
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import { Platform } from "react-native";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import Colors from "../constants/Colors";
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
     ProductDetail: ProductDetailScreen,
+    Cart: CartScreen,
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : "",
       },
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold",
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans",
+      },
       headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
     },
   }
 );
 
-export default createAppContainer(ProductsNavigator)
+export default createAppContainer(ProductsNavigator);
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -3,11 +3,13 @@ import {
   ScrollView,
   View,
   Text,
-  ImageComponent,
+  Image,
   Button,
   StyleSheet,
 } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Colors from "../../constants/Colors";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam("productId");
@@ -20,14 +22,47 @@ const ProductDetailScreen = (props) => {
       headerTitle: navData.navigation.getParam("productTitle"),
     };
   };
-
+  const dispatch = useDispatch();
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="Add to Cart"
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct));
+          }}
+        />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+    fontFamily: "open-sans-bold",
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20,
+    fontFamily: "open-sans",
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+});
 
 export default ProductDetailScreen;
